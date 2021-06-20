25b869efcd33a69f1db7cb525bd21bd6ffcc770f
hieunm292 <nmhieu2902@gmail.com>
2021-06-21 01:47:22 +0700

    finish Chapter-8

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,4 +18,14 @@ class User < ApplicationRecord
   before_save{email.downcase!}
 
   has_secure_password
+
+  def self.digest string
+    cost = if ActiveModel::SecurePassword.min_cost
+             BCrypt::Engine::MIN_COST
+           else
+             BCrypt::Engine.cost
+           end
+
+    BCrypt::Password.create string, cost: cost
+  end
 end
